3a2991fc495416e5a17db539dffe23dc120d0c88
CoffeeandData <carlos2romero@gmail.com>
2016-06-26 14:13:55 -0700

    updated robustD3.js to work D3.js V4

--- robustD3.js
+++ robustD3.js
@@ -44,46 +44,45 @@ setCanvas.prototype.scatterPlot = function(data, xValue, yValue){
 	graph = this.plot.select("g.mainGraph");
 	outline = this;
 
+	graphTrans = d3.transition().duration(1000);
+
 	rangeXValue = d3.extent(data, function(x) { return x[xValue]; });
 	rangeYValue = d3.extent(data, function(x) { return x[yValue]; });
 
 	graph.selectAll(".axis").remove();
-	var xScale = d3.scale.linear().range([0, outline.width]);
-	var yScale = d3.scale.linear().range([outline.height, 0]);
+	var xScale = d3.scaleLinear().range([0, outline.width]);
+	var yScale = d3.scaleLinear().range([outline.height, 0]);
 
 	//map data values (x,y) to graph scale
 	xScale.domain([rangeXValue[0]*.8, rangeXValue[1] *1.2]);
 	yScale.domain([rangeYValue[0]*.8, rangeYValue[1] *1.2]);
 
+
 	// Create Axis group
 	var xAxisGroup = graph.append("g").attr("transform", "translate(0, " + outline.height + ")").attr("class", "x axis");
 	var yAxisGroup = graph.append("g").attr("class", "y axis");
 
-	// Create Axis
-	var xAxis = d3.svg.axis().scale(xScale).orient("bottom");
-	var yAxis = d3.svg.axis().scale(yScale).orient("left");
+		
+	var xAxis = d3.axisBottom(xScale);
+	var yAxis = d3.axisLeft(yScale);
 
 	
 	// Call Axis
-	xAxisGroup.transition().duration(1000).call(xAxis);
-	yAxisGroup.transition().duration(1000).call(yAxis);
-	graph.selectAll(".axis line, .axis path").style({ "fill":"none", "stroke": "black", "shape-rendering":"crispEdges" });
-	graph.selectAll(".axis text").style({"font-family": "sans-serif", "font-size": "11px"});
-
-
+	xAxisGroup.transition(graphTrans).call(xAxis);
+	yAxisGroup.transition(graphTrans).call(yAxis);
+	
 	// Bind Data
 	var scatter = graph.selectAll(selectValue).data(data);
 	//Enter
 	scatter.enter().append(this.mainElement)
 		.attr("class", this.mainElementClass)
-		.attr("r", 0);
-
-	//Update
-	scatter
-		.transition().duration(1000)
-		.attr("r", 5) 
+		.attr("r", 0)
+	.merge(scatter)
+		.transition(graphTrans)
 		.attr("cx", function(x) { return xScale(x[xValue]); })
-		.attr("cy", function(x) { return yScale(x[yValue]); });
+		.attr("cy", function(x) { return yScale(x[yValue]); })
+		.attr("r", 5) 
+
 
 	//Exit
 	scatter.exit()
@@ -139,32 +138,28 @@ setCanvas.prototype.createTips = function(data, toolTipText){
 	colorScaleValue = this.colorScaleValue;
 	var graph = this.plot.select("g.mainGraph")
 	var selectors = d3.selectAll(this.mainElement);
-	
 	var divToolTip = d3.select("body").append("div").attr("class","divToolTip")
-		.style({
-			"opacity"        : "0", 
-			"position"       : "absolute",
-			"text-align"     : "center",			
-			"width"          :  "60px",					
-			"height"         :  "28px",					
-			"padding"        : "2px",		
-			"font"           : "12px sans-serif",		
-			"background"     : "lightsteelblue",	
-			"border"         : "0px",
-			"border-radius"  : "8px",			
-			"pointer-events" : "none"
-			});
+		.style("opacity" , "0").style("position",  "absolute").style("text-align", "center").style("width",  "60px")					
+		.style("height",  "28px").style("padding",  "2px").style("font",  "12px sans-serif")
+		.style("background",   "lightsteelblue").style("border", "0px").style("border-radius",  "8px")			
+		.style("pointer-events", "none");
+	
+		
 
 	selectors.on("mouseover", function (d) {
 		d3.select(this).attr("fill", "lightsteelblue");
-		divToolTip.transition().duration(500).style("opacity", ".9");
-		divToolTip.html(toolTipText+ ": " + d[toolTipText])
+		getDivToolTip = d3.select("div.divToolTip");
+		getDivToolTip.transition().duration(500).text(toolTipText + ": " + d[toolTipText])
 			.style("left", (d3.event.pageX) + "px")		
-			.style("top", (d3.event.pageY - 30) + "px");		
-        })
+			.style("top", (d3.event.pageY - 30) + "px")
+			.style("opacity", ".9");
+
+	       
+		})
 		.on("mouseout", function(d){
 			d3.select(this).attr("fill", colorScale(d[colorScaleValue]))
-			divToolTip.transition().duration(1000).style("opacity", 0);
+		getDivToolTip = d3.select("div.divToolTip");
+		getDivToolTip.transition().duration(1000).style("opacity", 0);
 		})
 }
 
@@ -179,4 +174,68 @@ groupByKey = function(data, mainKey){
 };
 
 
+// Barchart
 
+
+setCanvas.prototype.barChart = function(data, xValue, yValue){
+	this.mainElement = "rect";
+	this.mainElementClass = "barCharts"
+	var selectValue = this.mainElement + "." + this.mainElementClass;
+	var graph = this.plot.select("g.mainGraph");
+	var outline = this;
+	console.log(this);
+
+
+	rangeXValue = d3.extent(data, function(x) { return x[xValue]; });
+	rangeYValue = d3.extent(data, function(x) { return x[yValue]; });
+
+	graph.selectAll(".axis").remove();
+	var xScale = d3.scaleLinear().range([0, outline.width]);
+	var yScale = d3.scaleLinear().range([outline.height, 0]);
+
+	//map data values (x,y) to graph scale
+	xScale.domain([rangeXValue[0]*.8, rangeXValue[1] *1.2]);
+	yScale.domain([rangeYValue[0]*.8, rangeYValue[1] *1.2]);
+
+
+	// Create Axis group
+	var xAxisGroup = graph.append("g").attr("transform", "translate(0, " + outline.height + ")").attr("class", "x axis");
+	var yAxisGroup = graph.append("g").attr("class", "y axis");
+
+	// Create Axis
+	var xAxis = d3.axisBottom(xScale);
+	var yAxis = d3.axisLeft(yScale);
+
+		
+	// Call Axis
+	xAxisGroup.transition().duration(1000).call(xAxis);
+	yAxisGroup.transition().duration(1000).call(yAxis);
+
+
+	var	bars = graph.selectAll(selectValue).data(data);
+	bars.enter()
+		.append(this.mainElement)
+		.attr("class", this.mainElementClass)
+		.attr("width", 15)
+	.merge(bars)
+		.attr("y", function(x) { return yScale(x[yValue]); })	//.attr("height", function(x) { return outline.height - yScale(x[yValue]); })
+		.attr("height", function(x) { return  outline.height - yScale(x[yValue]); })
+		.attr("x", function(d, i) {return i  * 20; })
+}
+
+
+getFreq = function(data, variable){
+				freqObj = {}
+				data.forEach(function(d){ 
+					var getValue = d[variable];
+					freqObj[getValue] = freqObj[getValue] == undefined ? 1 : freqObj[getValue] + 1;
+				})
+				
+				var newArray = Object.keys(freqObj).map(function(key) { 
+					var tempObj = {};
+					tempObj[variable] = key
+					tempObj["Freq"]   = freqObj[key]
+					return tempObj;
+				});
+				return newArray;
+			}
